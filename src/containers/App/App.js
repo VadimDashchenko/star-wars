989bdeb868272053c6214499aa85a99e201f8330
VadimDashchenko <dashchenko.vad.92@gmail.com>
2020-10-30 09:51:58 +0200

    created cart item for the planet and recomposed file structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CardItem from '../../components/CardItem/CardItem';
+
+const App = () => {
+  return (
+    <div className="App">
+        <CardItem />
+    </div>
+  );
+}
+
+export default App;
